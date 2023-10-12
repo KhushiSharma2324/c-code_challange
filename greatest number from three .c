c04e2d56957ff2202c98525ab983fd09c92c1ef5
khushi sharma <146974496+KhushiSharma2324@users.noreply.github.com>
2023-10-13 00:55:35 +0530

    Add files via upload

---
+++ greatest number from three .c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c;
+ 
+ printf("enter any three values");
+ 
+ scanf("%d%d%d",&a,&b,&c);
+ 
+ if (a>b && a>c)	
+ {
+ 	printf("a is greater number",a);
+ }
+ else if (b>a && b>c)
+ {
+ 	printf("b is greater number",b);
+ }
+ else (c>a && c>b)
+      {
+ 	printf("c is greater number",c);
+      }	
+return 0;	
+	
+}
+	
+	
+	
+	
+	
+	
+	
+
